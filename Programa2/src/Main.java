5b25744deceba412e9c46980c4a75672f1fd1818
javii.tc <itsjavii1610@gmail.com>
2025-10-08 10:52:30 +0200

    Codigo corregido

--- Programa2/src/Main.java
+++ Programa2/src/Main.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    // El código pide un numero del 1 al 10 para ver si coincide con el numero x, el cual es generado aleatoriamente, si el numero introducido coincide con x,
+    //se considera que ha ganado el premio (como si fuera un sorteo), si no coinciden muestra un mensaje agradeciendo por participar, y si se introduce un valor
+    //mayor que 10, avisará que el número no es válido
     public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     int x = (int)(Math.random() * 10 + 1);
+        System.out.println("Introduzca un numero del 1 al 10");
     int y = 0;
+    String frase = "Enhorabuena, usted ha obtenido el premio";
+    String frase2 = "El numero introducido no coincide con el numero ganador, gracias por participar!";
+    String frase3 = "Numero no valido, intentelo de nuevo";
     while (y != x) {
         y = s.nextInt();
         if (y == x)
-            System.out.println("i");
-        else if (y > x)
-            System.out.println("e");
+            System.out.println(frase);
+        else if (y > 10)
+            System.out.println(frase3);
+        else if (y != x)
+            System.out.println(frase2);
         else
-            System.out.println("a");
+            System.out.println(frase3);
+        break;
     }
 }}
--- Programa3/src/Main.java
+++ Programa3/src/Main.java
@@ -1,52 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
+    // El siguiente código actua como si fuera un cajero (muy simple y con operaciones limitadas), el cual segun la acción que le indiques, podrás mover dinero
+    // a determinadas cuentas, consultar el dinero disponible en las diferentes cuentas, o terminar de operar.
+    // En caso de seleccionar una operación no disponible (un numero mayor que 5), saltará un mensaje de que la operación no es válida, y que se
+    // seleccione una que si lo sea.
+    // Si la cuenta principal 'g' llega a 0, automaticamente saltará un mensaje de saldo insuficiente, mostrará el dinero disponible en las cuentas y terminará
+    // el programa.
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int g = 50;
-        int h = 0;
-        int a = 0;
-        int b = 0;
-        int c = 0;
+        int g = 50; // saldo disponible cuenta personal/facturas
+        int e = 0; //saldo disponible, cuenta de empresa
+        int a = 0; // saldo disponible, cuenta de ahorro
+        int i = 0; // saldo disponible cuenta de inversion
+        int c = 0; //saldo disponible para gastos personales
         boolean x = true;
         while (x) {
+            System.out.println("Seleccione 1 para mover 10€ de su cuenta principal e ingresarlo equitativamente a sus cuentas de empresa y ahorro");
+            System.out.println("Seleccione 2 para mover 7€ de su cuenta principal e ingresar 3€ en su cuenta de empresa y 4€ a inversión");
+            System.out.println("Seleccione 3 para mover 20€ de su cuenta principal e ingresarlo equitativamente en su cuenta de empresa e inversión ");
+            System.out.println("Seleccione 4 para consultar sus cuentas");
+            System.out.println("Seleccione 5 para salir de las operaciones");
             System.out.println("1 2 3 4 5");
             int n = s.nextInt();
             if (n == 1) {
-                if (g > 10) {
+                if (g >= 10) {
                     g = g - 10;
-                    h = h + 5;
-                    a = a + 1;
+                    e = e + 5;
+                    a = a + 5;
                 } else {
-                    System.out.println("x");
+                    System.out.println("Saldo insuficiente para continuar con la operación");
                 }
             } else if (n == 2) {
-                if (g > 7) {
+                if (g >= 7) {
                     g = g - 7;
-                    h = h + 3;
-                    b = b + 1;
+                    e = e + 3;
+                    i = i + 4;
                 } else {
-                    System.out.println("x");
+                    System.out.println("Saldo insuficiente para continuar con la operación");
                 }
             } else if (n == 3) {
-                if (g > 20) {
+                if (g >= 20) {
                     g = g - 20;
-                    h = h + 10;
-                    c = c + 1;
+                    e = e + 10;
+                    c = c + 10;
                 } else {
-                    System.out.println("x");
+                    System.out.println("Saldo insuficiente para continuar con la operación");
                 }
             } else if (n == 4) {
-                System.out.println(g);
-                System.out.println(h);
-                System.out.println(a);
-                System.out.println(b);
-                System.out.println(c);
+                System.out.println("Cuenta personal: " + g +"€");
+                System.out.println("Cuenta de empresa: " + e + "€");
+                System.out.println("Cuenta de ahorro: " + a + "€");
+                System.out.println("Cuenta de inversion: " + i + "€");
+                System.out.println("Cuenta de gastos personales" + c + "€");
             } else if (n == 5) {
                 x = false;
+            } else if (n > 5) {
+                System.out.println("Seleccione una operación válida");
             }
-            if (g < 0) {
-                System.out.println("end");
+            if (g == 0) {
+                System.out.println("Saldo insuficiente en su cuenta principal");
+                System.out.println("Cuenta personal: " + g +"€");
+                System.out.println("Cuenta de empresa: " + e + "€");
+                System.out.println("Cuenta de ahorro: " + a + "€");
+                System.out.println("Cuenta de inversion: " + i + "€");
+                System.out.println("Cuenta de gastos personales" + c + "€");
                 x = false;
             }
         }
