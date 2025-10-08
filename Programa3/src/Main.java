8fba74211a8d307d6dc5f25803a0189982282823
javii.tc <itsjavii1610@gmail.com>
2025-10-08 11:24:42 +0200

    Codigo corregido

--- Programa3/src/Main.java
+++ Programa3/src/Main.java
@@ -15,13 +15,13 @@ public class Main {
         int i = 0; // saldo disponible cuenta de inversion
         int c = 0; //saldo disponible para gastos personales
         boolean x = true;
+        System.out.println("Seleccione 1 para mover 10€ de su cuenta principal e ingresarlo equitativamente a sus cuentas de empresa y ahorro");
+        System.out.println("Seleccione 2 para mover 7€ de su cuenta principal e ingresar 3€ en su cuenta de empresa y 4€ a inversión");
+        System.out.println("Seleccione 3 para mover 20€ de su cuenta principal e ingresarlo equitativamente en su cuenta de empresa e inversión ");
+        System.out.println("Seleccione 4 para consultar sus cuentas");
+        System.out.println("Seleccione 5 para salir de las operaciones");
+        System.out.println("1 2 3 4 5");
         while (x) {
-            System.out.println("Seleccione 1 para mover 10€ de su cuenta principal e ingresarlo equitativamente a sus cuentas de empresa y ahorro");
-            System.out.println("Seleccione 2 para mover 7€ de su cuenta principal e ingresar 3€ en su cuenta de empresa y 4€ a inversión");
-            System.out.println("Seleccione 3 para mover 20€ de su cuenta principal e ingresarlo equitativamente en su cuenta de empresa e inversión ");
-            System.out.println("Seleccione 4 para consultar sus cuentas");
-            System.out.println("Seleccione 5 para salir de las operaciones");
-            System.out.println("1 2 3 4 5");
             int n = s.nextInt();
             if (n == 1) {
                 if (g >= 10) {
@@ -54,6 +54,7 @@ public class Main {
                 System.out.println("Cuenta de inversion: " + i + "€");
                 System.out.println("Cuenta de gastos personales" + c + "€");
             } else if (n == 5) {
+                System.out.println("Cerrando sesión, gracias por usar nuestros servicios!");
                 x = false;
             } else if (n > 5) {
                 System.out.println("Seleccione una operación válida");
--- Programa4/src/Main.java
+++ Programa4/src/Main.java
@@ -10,12 +10,19 @@ public class Main {
         boolean a = true;
 
         while (a) {
+            System.out.println("Seleccione el objeto a usar");
+            System.out.println("1- Piedra de ira: Aumenta el poder de la ira espartana a cambio de salud");
+            System.out.println("2- Piedra de salud S: Recupera 10 puntos de HP");
+            System.out.println("3- Piedra de salud M: Recupera todos los puntos de HP");
+            System.out.println("4- Hoja del Olimpo: ");
+            System.out.println("0- Salir del menú");
             int o = s.nextInt();
 
             if (o == 0) break;
 
+
             if (o == 1) {
-                v -= 3 + r.nextInt(5);
+                v -= 3 - r.nextInt(5);
                 x += 5 + r.nextInt(7);
                 if (x >= 50 && x % 50 < 10) d += 2;
             }
